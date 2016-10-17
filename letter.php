3003a5221ca507f2dab11c5ea5afd9531042d233
polina <polina.mihova@yahoo.com>
2016-10-17 11:36:02 -0700

    create php variable

--- letter.php
+++ letter.php
@@ -1,3 +1,7 @@
+<?php
+    $friend_name = "Jennifer";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,10 +9,10 @@
 </head>
 <body>
     <h1>Greetings From Afar</h1>
-    <p>Dear Jennifer,</p>
-    <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming! </p>
-    <p>Jennifer, you would not believe how cold it is here. I should have gone to Hawaii instead. But I like programming a lot, so I've got that going for me. </p>
-    <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
+    <p>Dear <?php echo "Jennifer"; ?>,</p>
+    <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming!</p>
+    <p><?php echo "Jennifer"; ?>, you would not believe how cold it is here. I should have gone to Hawaii instead. But I like programming a lot, so I've got that going for me.</p>
+    <p>Looking forward to seeing you soon. I'll bring you back a souvenir.</p>
     <p>Cheers,</p>
     <p>Diane</p>
 </body>
