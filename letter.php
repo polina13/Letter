25bf728a5b022fc59a66c0cba61b1837b273099e
polina <polina.mihova@yahoo.com>
2016-10-17 11:40:23 -0700

    create example php file for practice

--- letter.php
+++ letter.php
@@ -1,5 +1,5 @@
 <?php
-    $friend_name = "Jennifer";
+    $friend_name = "Max";
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,9 @@
 </head>
 <body>
     <h1>Greetings From Afar</h1>
-    <p>Dear <?php echo "Jennifer"; ?>,</p>
+    <p>Dear <?php echo $friend_name; ?>,</p>
     <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming!</p>
-    <p><?php echo "Jennifer"; ?>, you would not believe how cold it is here. I should have gone to Hawaii instead. But I like programming a lot, so I've got that going for me.</p>
+    <p><?php echo $friend_name; ?>, you would not believe how cold it is here. I should have gone to Hawaii instead. But I like programming a lot, so I've got that going for me.</p>
     <p>Looking forward to seeing you soon. I'll bring you back a souvenir.</p>
     <p>Cheers,</p>
     <p>Diane</p>
---
+++ order_template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Order Form</title>
+</head>
+<body>
+    <p>Hi Ed, </p>
+    <p>Just making my weekly order. Details are below.</p>
+    <p>300 Cups</p>
+    <p>208 SW 5th Ave #105, Portland, OR 97204</p>
+    <p>500 Cups</p>
+    <p>1978 19th Ave, Portland, OR 97209</p>
+    <p>1000 Cups</p>
+    <p>1493 SW 2nd Ave, Portland, OR 97201</p>
+    <p>Thanks,</p>
+    <p>Diane</p>
+</body>
+</html>
